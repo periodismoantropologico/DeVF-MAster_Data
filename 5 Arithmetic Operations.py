b00409064558a4c030e6111269a89490093b69f8
psylocybe <63525520+periodismoantropologico@users.noreply.github.com>
2021-02-04 18:43:31 -0600

    Add files via upload

---
+++ 5 Arithmetic Operations.py
@@ -0,0 +1,13 @@
+print (10 ** 3)
+x = 10
+x = x + 3
+print(x)
+x -= 3
+print(x)
+##
+x = 10 +3 *2
+print(x)
+#parenthesis always is a priority
+#exponentiation 2 ** 3
+#multiplication or division
+#addition or subtraction
